88039687c2065728c1301568e6c0ded3b5b41728
Marco Donna <marco.donna03@gmail.com>
2022-11-25 09:41:50 +0100

    Linear regression

---
+++ linearRegression/src/main/java/ai/LinearRegression.java
@@ -0,0 +1,33 @@
+package ai;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class LinearRegression{
+    ArrayList<Double []> data = new ArrayList<Double []>();
+    
+    public LinearRegression setData(ArrayList<Double []> data){
+        this.data = data;  
+        return this;
+    }
+
+    public HashMap<String, Double> fit(int epochs, double learningRate){
+        double weight = 1, bias = 0;
+        for(int i = 0; i < epochs; i++){
+            double derivativeW = 0, derivativeB = 0;
+            for(Double[] row: data){
+                derivativeW += row[0] * (weight * row[0] + bias - row[1]);
+                derivativeB += weight * row[0] + bias - row[1];
+            }
+            if(i%1000 == 0)
+                System.out.println("" + derivativeW + ":" + derivativeB + ":" + data.size());
+            weight -= learningRate*(derivativeW/data.size());
+            bias -= learningRate*(derivativeB/data.size());
+        }
+
+        HashMap<String, Double> ret = new HashMap<String, Double>();
+        ret.put("weight", weight);
+        ret.put("bias", bias);
+        return ret;
+    }
+}
